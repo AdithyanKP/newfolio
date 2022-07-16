85b34a64f8736c55e9c730e7afb1b3f61380ff73
Adithyan k P <adithyankp777@gmail.com>
2022-07-16 22:47:13 +0530

    home ui implemented

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,11 @@
 import React, { useState } from "react";
-import { FaBars, FaTimes } from "react-icons/fa";
+import {
+  FaBars,
+  FaTimes,
+  FaLinkedin,
+  FaGithub,
+  FaInstagram,
+} from "react-icons/fa";
 const Navbar = () => {
   const [nav, setNav] = useState(false);
 
@@ -33,12 +39,43 @@ const Navbar = () => {
             : " md:hidden absolute top-0 left-0 flex flex-col w-full h-screen justify-center align-center bg-[#0a192f] items-center"
         }
       >
-        <li className="py-4 text-4xl">Home</li>
-        <li className="py-4 text-4xl">About</li>
-        <li className="py-4 text-4xl">Skills</li>
-        <li className="py-4 text-4xl">Work</li>
-        <li className="py-4 text-4xl">Contact</li>
+        <li className="py-4 text-3xl">Home</li>
+        <li className="py-4 text-3xl">About</li>
+        <li className="py-4 text-3xl">Skills</li>
+        <li className="py-4 text-3xl">Work</li>
+        <li className="py-4 text-3xl">Contact</li>
       </ul>
+
+      {/* social icons */}
+      <div className=" hidden lg:flex fixed flex-col top-[35%] left-0">
+        <ul>
+          <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-blue-600">
+            <a
+              href="/"
+              className="flex justify-between items-center w-full text-gray-300"
+            >
+              LinkedIn <FaLinkedin size={30} />
+            </a>
+          </li>
+          <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#333333]">
+            <a
+              href="/"
+              className="flex justify-between items-center w-full text-gray-300"
+            >
+              Github <FaGithub size={30} />
+            </a>
+          </li>
+          <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#00cc88]">
+            <a
+              href="/"
+              className="flex justify-between items-center w-full text-gray-300"
+            >
+              Instagram
+              <FaInstagram size={30} />
+            </a>
+          </li>
+        </ul>
+      </div>
     </div>
   );
 };
