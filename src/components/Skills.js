b826d1d5fdf7f887bbf4c76d23a6f5e63a7ee3fa
Adithyan k P <adithyankp777@gmail.com>
2022-07-17 00:07:58 +0530

    changes

---
+++ src/components/Skills.js
@@ -0,0 +1,47 @@
+import React from "react";
+import node from "../assets/node.png";
+import react from "../assets/react.png";
+import javascript from "../assets/javascript.png";
+import git from "../assets/github.png";
+import tailwind from "../assets/tailwind.png";
+
+function Skills() {
+  return (
+    <div name="skills" className="w-full h-screen bg-[#0a192f] text-gray-300">
+      {/* Container */}
+      <div className="max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full">
+        <div>
+          <p className="text-4xl font-bold inline border-b-4 border-pink-600 ">
+            Skills
+          </p>
+        </div>
+
+        <div className="w-full grid grid-cols-2 sm:grid-cols-4 gap-4 text-center py-8 my-4">
+          <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
+            <img className="w-20 mx-auto" src={javascript} alt="HTML icon" />
+            <p className="my-4">JAVASCRIPT</p>
+          </div>
+          <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
+            <img className="w-20 mx-auto" src={react} alt="HTML icon" />
+            <p className="my-4">REACT</p>
+          </div>
+          <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
+            <img className="w-20 mx-auto" src={git} alt="HTML icon" />
+            <p className="my-4">GITHUB</p>
+          </div>
+          <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
+            <img className="w-20 mx-auto" src={node} alt="HTML icon" />
+            <p className="my-4">NODE JS</p>
+          </div>
+
+          <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
+            <img className="w-20 mx-auto" src={tailwind} alt="HTML icon" />
+            <p className="my-4">Tailwind</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Skills;
