31479e554341afb3b7909e4489c312f6c172b23a
Adithyan k P <adithyankp777@gmail.com>
2022-07-17 15:02:19 +0530

    fix

--- src/components/Work.js
+++ src/components/Work.js
@@ -8,15 +8,18 @@ import quiz from "../assets/quiz.png";
 
 const Work = () => {
   return (
-    <div name="work" className="w-full md:h-screen text-gray-300  bg-[#0a192f]">
-      <div className="max-w-[1000px] mx-auto p-4  flex flex-col justify-center w-full h-full">
-        <div>
+    <div
+      name="work"
+      className="w-full md:h-screen text-gray-300  bg-[#0a192f] "
+    >
+      <div className="max-w-[1000px] mx-auto p-4  flex flex-col justify-center w-full h-full ">
+        <div className="my-4">
           <p className="text-4xl font-bold inline border-b-4 text-gray-300 border-pink-600">
             Work
           </p>
         </div>
         {/* card item */}
-        <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-4 my-8">
+        <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-4 my-2">
           <div
             style={{
               backgroundImage: `url(${covid})`,
