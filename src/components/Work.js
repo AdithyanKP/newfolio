698349d584c9d2b3b3599674e62ad920d078fe60
Adithyan k P <adithyankp777@gmail.com>
2022-07-17 14:40:51 +0530

    changes

--- src/components/Work.js
+++ src/components/Work.js
@@ -1,6 +1,11 @@
 import React from "react";
 import eplant from "../assets/eplant.png";
 import netflix from "../assets/netflix.png";
+import covid from "../assets/covid-19.png";
+import todo from "../assets/todo.png";
+import logus from "../assets/logus.png";
+import quiz from "../assets/quiz.png";
+
 const Work = () => {
   return (
     <div name="work" className="w-full md:h-screen text-gray-300  bg-[#0a192f]">
@@ -14,16 +19,16 @@ const Work = () => {
         <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-4 my-8">
           <div
             style={{
-              backgroundImage: `url(${eplant})`,
+              backgroundImage: `url(${covid})`,
               backgroundBlendMode: "darken",
-              backgroundColor: "rgba(0, 0, 0, 0.5)",
+              backgroundColor: "rgba(0, 0, 0, 0.4)",
             }}
             className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
           >
             {/* {Hover effectss */}
             <div className="opacity-0 group-hover:opacity-100">
               <span className="text-2xl font-bold text-white tracking-wider">
-                E-Plant ecommerce
+                Covid-19 tracker
               </span>
               <div className="pt-8 text-center">
                 <a href="/">
@@ -41,13 +46,129 @@ const Work = () => {
           </div>
 
           <div
-            style={{ backgroundImage: `url(${netflix})` }}
+            style={{
+              backgroundImage: `url(${eplant})`,
+              backgroundBlendMode: "darken",
+              backgroundColor: "rgba(0, 0, 0, 0.4)",
+            }}
+            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
+          >
+            {/* {Hover effectss */}
+            <div className="opacity-0 group-hover:opacity-100">
+              <span className="text-2xl font-bold text-white tracking-wider">
+                E-Plant Ecommerce
+              </span>
+              <div className="pt-8 text-center">
+                <a href="/">
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    Demo
+                  </button>
+                </a>
+                <a href="/">
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    GitHub
+                  </button>
+                </a>
+              </div>
+            </div>
+          </div>
+
+          <div
+            style={{
+              backgroundImage: `url(${logus})`,
+              backgroundBlendMode: "darken",
+              backgroundColor: "rgba(0, 0, 0, 0.4)",
+            }}
+            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
+          >
+            {/* {Hover effectss */}
+            <div className="opacity-0 group-hover:opacity-100">
+              <span className="text-2xl font-bold text-white tracking-wider">
+                Logus Social-media App
+              </span>
+              <div className="pt-8 text-center">
+                <a href="/">
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    Demo
+                  </button>
+                </a>
+                <a href="/">
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    GitHub
+                  </button>
+                </a>
+              </div>
+            </div>
+          </div>
+
+          <div
+            style={{
+              backgroundImage: `url(${netflix})`,
+              backgroundBlendMode: "darken",
+              backgroundColor: "rgba(0, 0, 0, 0.4)",
+            }}
             className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
           >
             {/* {Hover effectss */}
             <div className="opacity-0 group-hover:opacity-100">
               <span className="text-2xl font-bold text-white tracking-wider">
                 Netflix clone
+              </span>
+              <div className="pt-8 text-center">
+                <a href="/">
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    Demo
+                  </button>
+                </a>
+                <a href="/">
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    GitHub
+                  </button>
+                </a>
+              </div>
+            </div>
+          </div>
+
+          <div
+            style={{
+              backgroundImage: `url(${todo})`,
+              backgroundBlendMode: "darken",
+              backgroundColor: "rgba(0, 0, 0, 0.4)",
+            }}
+            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
+          >
+            {/* {Hover effectss */}
+            <div className="opacity-0 group-hover:opacity-100">
+              <span className="text-2xl font-bold text-white tracking-wider">
+                Todo App
+              </span>
+              <div className="pt-8 text-center">
+                <a href="/">
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    Demo
+                  </button>
+                </a>
+                <a href="/">
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    GitHub
+                  </button>
+                </a>
+              </div>
+            </div>
+          </div>
+
+          <div
+            style={{
+              backgroundImage: `url(${quiz})`,
+              backgroundBlendMode: "darken",
+              backgroundColor: "rgba(0, 0, 0, 0.4)",
+            }}
+            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
+          >
+            {/* {Hover effectss */}
+            <div className="opacity-0 group-hover:opacity-100">
+              <span className="text-2xl font-bold text-white tracking-wider">
+                Simple quiz App
               </span>
               <div className="pt-8 text-center">
                 <a href="/">
