ac46deed96752a09b10335a3feff5e2dc421ee8a
Adithyan k P <adithyankp777@gmail.com>
2022-07-19 22:53:51 +0530

    changes

--- src/components/Work.js
+++ src/components/Work.js
@@ -64,11 +64,6 @@ const Work = () => {
               <div className="pt-8 text-center">
                 <a href="/">
                   <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
-                    Demo
-                  </button>
-                </a>
-                <a href="/">
-                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
                     GitHub
                   </button>
                 </a>
